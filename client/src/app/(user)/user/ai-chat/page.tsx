2bb8382640e4a478ee6e22e8446da13a97465f27
ahmed Bakur <ahhmadas213@gmail.com>
2024-03-24 22:24:25 +0300

    ai

--- client/src/app/(user)/user/ai-chat/page.tsx
+++ client/src/app/(user)/user/ai-chat/page.tsx
@@ -4,32 +4,35 @@ import { useChat } from 'ai/react';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Button } from '@/components/ui/button';
+import PageTitle from "@/components/PageTitle"
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
   return (
-    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-      {messages.map(m => (
-        <div key={m.id} className="whitespace-pre-wrap">
-          {m.role === 'user' ? 'User: ' : 'AI: '}
-          {m.content}
-        </div>
-      ))}
+    <div className='p-6'>
+      <PageTitle title='المساعد الذكي' />
+      <div className="flex flex-col  w-full max-w-md py-24 mx-auto stretch text-xl gap-2">
+        {messages.map(m => (
+          <div key={m.id} className="whitespace-pre-wrap">
+            {m.role === 'user' ? 'User: ' : 'AI: '}
+            {m.content}
+          </div>
+        ))}
 
-      <form onSubmit={handleSubmit}>
-        <Label className='fixed flex flex-wrap items-center mx-auto justify-center rounded-3xl bottom-0 md:w-full max-w-md left-1/2 -translate-x-1/2 mb-8 border border-gray-300 shadow-xl'>
-          <Button type='submit' className='rounded-tl-none rounded-bl-none outline-none'>sumbit</Button>
-          <Input
-            className="w-full h-full flex-1 rounded-tr-none rounded-br-none outline-none "
-            value={input}
-            placeholder="مرحبا ..."
-            onChange={handleInputChange}
-          />
+        <form onSubmit={handleSubmit} className='w-full relative '>
+          <Label className=' flex w-full flex-wrap items-center  mx-auto justify-center 
+          rounded-3xl bottom-0 md:w-full left-0 md:left-auto max-w-md fixed mb-8 border border-gray-300 shadow-xl'>
+            <Button type='submit' className='rounded-tl-none rounded-bl-none outline-none'>sumbit</Button>
+            <Input
+              className="w-full h-full  flex-1 rounded-tr-none rounded-br-none outline-none "
+              value={input}
+              placeholder="مرحبا ..."
+              onChange={handleInputChange}
+            />
 
-
-        </Label>
-
-
-      </form>
+          </Label>
+        </form>
+      </div>
     </div>
+
   );
 }

69a28c23feeef6e21ab41e82827b6597697126f0
Mohamed_Hegazy <asadmoahmed405@gmail.com>
2024-03-28 21:04:22 +0200

    remove field DateOfEstablishmentOfInstitution from org

--- backend/src/Dto/org/create.org.dto.ts
+++ backend/src/Dto/org/create.org.dto.ts
@@ -1,15 +1,18 @@
 import {
-  IsDateString,
+  // IsDateString,
   IsEmail,
   IsNotEmpty,
   IsString,
   IsUrl,
+  Matches,
   MaxLength,
   MinLength,
 } from 'class-validator';
 import { Signup } from '../shared-Dto/org-user.dto';
-import { IsDateInRange } from 'shared/functions/data.range';
-
+// import { IsDateInRange } from 'shared/functions/data.range';
+const customEmailRegex = new RegExp(
+  '^[a-zA-Z0-9._%+-]+@(?!gmail.com)(?!yahoo.com)(?!hotmail.com)(?!yahoo.co.in)(?!aol.com)(?!live.com)(?!outlook.com)[a-zA-Z0-9_-]+.[a-zA-Z0-9-.]{2,61}$',
+);
 export class CreateOrgDto extends Signup {
   @IsNotEmpty()
   @IsString()
@@ -18,7 +21,10 @@ export class CreateOrgDto extends Signup {
   Name: string;
 
   @IsNotEmpty()
-  @IsEmail()
+  @IsEmail({}, { message: 'Invalid email format' })
+  @Matches(customEmailRegex, {
+    message: 'Email domain is not allowed',
+  })
   Email: string;
 
   @IsNotEmpty()
@@ -30,10 +36,10 @@ export class CreateOrgDto extends Signup {
   @IsString()
   Website: string;
 
-  @IsDateString()
-  @IsNotEmpty()
-  @IsDateInRange(1960, 2024, {
-    message: 'The Date must be between 1980 and 2021.',
-  })
-  DateOfEstablishmentOfInstitution: Date;
+  // @IsDateString()
+  // @IsNotEmpty()
+  // @IsDateInRange(1960, 2024, {
+  //   message: 'The Date must be between 1980 and 2021.',
+  // })
+  // DateOfEstablishmentOfInstitution: Date;
 }

56cfcb439224801da2cbcb7f04918bc711da3c67
Mohamed_Hegazy <asadmoahmed405@gmail.com>
2024-03-28 21:53:13 +0200

    append endpoint for add orders to user

--- backend/src/db/schemas/package.schema.ts
+++ backend/src/db/schemas/package.schema.ts
@@ -1,6 +1,8 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
 import { Commodity } from 'types/package.interface';
+import { Order } from './order.schema';
+import { User } from './user.schema';
 
 export type PackageDocument = HydratedDocument<Package>;
 
@@ -14,6 +16,12 @@ export class Package implements Commodity {
 
   @Prop({ required: true, type: String, minlength: 3, maxlength: 50 })
   Description: string;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
+  Owner: User;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Order' })
+  Order: Order;
 
   @Prop({ type: Date, default: Date.now })
   CreatedAt: Date;
